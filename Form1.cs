6a8b9dd813e76c7df0405839633a8adec2ee77a6
root <root@v1645578.hosted-by-vdsina.ru>
2023-02-04 22:30:40 +0300

    first commit

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace FileReader
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> list = new List<string>();
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            string path = @"C:\Users\Администратор\Desktop\file.txt";
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+
+                while ((s = sr.ReadLine()) != null)
+                {
+                    listBox1.Items.Add(s); // добавляем полученные данные в ListBox  
+
+                    list.Add(s); // добавляем полученные данные в List 
+
+                }
+
+            }
+
+        }
+
+    }
+}
